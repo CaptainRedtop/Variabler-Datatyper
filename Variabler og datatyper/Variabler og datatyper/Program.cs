8e9fd452b331293c4b1a3ed3e69ab17fc9882b8e
CaptainRedtop <134284651+CaptainRedtop@users.noreply.github.com>
2023-08-10 11:04:44 +0200

    Update Program.cs

--- Variabler og datatyper/Variabler og datatyper/Program.cs
+++ Variabler og datatyper/Variabler og datatyper/Program.cs
@@ -10,9 +10,9 @@ namespace Variabler_og_datatyper
             int length = 16;
             double width = 5.1;
             //Opgave b
-            int area = width * length; //Kan ikke lade sig gøre fordi en int ikke kan indeholde en double.
+            int area = width * length; //Kan ikke lade sig gøre fordi en int ikke kan indeholde en double(Kommatal).
             //Opgave c
-            double d = width * length; //Kan lade sig gøre fordi double godt må indeholde en int.
+            double d = width * length; //Kan lade sig gøre fordi double godt kan indeholde en int(Heltal).
 
             Console.WriteLine(length * width);
             Console.ReadLine();
