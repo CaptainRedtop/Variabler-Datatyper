4285f332e34950f2b6f3d21a3d31aa3a935f4eb9
CaptainRedtop <134284651+CaptainRedtop@users.noreply.github.com>
2023-08-10 09:58:23 +0200

    Added opgave

---
+++ Variabler og datatyper/Variabler og datatyper/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Variabler_og_datatyper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Opgave a
+            int length = 16;
+            double width = 5.1;
+            //Opgave b
+            int area = width * length; //Kan ikke lade sig gøre fordi en int ikke kan indeholde en double.
+            //Opgave c
+            double d = width * length; //Kan lade sig gøre fordi double godt må indeholde en int.
+
+            Console.WriteLine(length * width);
+            Console.ReadLine();
+        }
+    }
+}
